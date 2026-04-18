90f2d07f631d3c473d6b24d62749fe3df66be4f8
Anxhhhh <anshrajsingh62@gmail.com>
2026-04-18 23:16:18 +0530

    hey

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from "express"
+import { connectdb } from "./config/db";
 let app = express();
 
 let port = 3000;
+
+connectdb();
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
